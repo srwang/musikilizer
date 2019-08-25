59f17e140f4eff5d799d7344daea39d152d85b3e
Sharon Wang <sharon@pwvconsultants.com>
2019-08-25 11:05:02 +0300

    logic for changing text into music

--- client/src/Home/home.js
+++ client/src/Home/home.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from "react";
 import { TextField, Button } from '@material-ui/core';
+import Tone from 'tone';
 
 class Home extends Component {
     constructor(props) {
@@ -7,9 +8,11 @@ class Home extends Component {
         this.state = { 
             apiResponse: "",
             textContent: "", 
+            musicReady: false,
+            notes: []
         };
         this.handleChange = this.handleChange.bind(this);
-        this.makeMusic = this.makeMusic.bind(this);
+        this.changeWordsToNotes = this.changeWordsToNotes.bind(this);
     }
 
     handleChange = type => event => {
@@ -17,16 +20,60 @@ class Home extends Component {
         this.setState({ [type]: value })
     }
 
-    makeMusic = () => {
-        const synth = new Tone.MembraneSynth().toMaster();
-        const words = this.state.textContent.split(" ");
-        let notes = words.map(word => {
+    changeWordsToNotes = () => {
+        const words = this.state.textContent.split(/[.,!?:;\"-]|\s/);
+        const startLetterToNote = { a: 'C1', b: 'D1', c: 'E1', d: 'F1', e: 'G1', f: 'A1', g: 'B1', h: 'C2', i: 'D2', j: 'E2', k: 'F2', l: 'G2', m: 'A2', n: 'B2', o: 'C3', p: 'D3', q: 'E3', r: 'F3', s: 'G3', t: 'A3', u: 'B3', v: 'C4', w: 'D4', x: 'E4', y: 'F4', z: 'G4' }
 
+        let notes = words.map(word => {
+            if (word.length === 0) return null;
+
+            word = word.toLowerCase();
+            let note = startLetterToNote[word[0]];
+
+            let lastLetter = word[word.length-1]
+
+            if (['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'].includes(lastLetter)) {
+                note = note.split('');
+                note.splice(1, 0, 'b');
+                note = note.join('');
+            } else if (['q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'].includes(lastLetter)) {
+                note = note.split('');
+                note.splice(1, 0, '#');
+                note = note.join('');
+            }
+            return note;
         })
-        //make words into notes
-        //when it is done, buttons should read "play" and "reset"
-        //play
-        //reset
+        this.setState({notes: notes});
+        this.setState({musicReady: true});
+    }
+
+    playMusic = () => {
+        const synth = new Tone.MembraneSynth().toMaster();
+        const notes = this.state.notes;
+
+        const synthPart = new Tone.Sequence(
+          function(time, note) {
+            synth.triggerAttackRelease(note, "10hz", time);
+          },
+          notes,
+          "4n"
+        );
+
+        synthPart.loop = 0;
+
+        synthPart.start();
+
+        Tone.Transport.start();
+    }
+
+    stopMusic = () => {
+        Tone.Transport.stop();
+    }
+
+    resetMusic = () => {
+        this.setState({notes: []});
+        this.setState({textContent: ""});
+        this.setState({musicReady: false});
     }
 
     callAPI() {
@@ -50,11 +97,36 @@ class Home extends Component {
                     onChange={this.handleChange('textContent')}
                 >
                 </TextField>
-                <Button
-                    onClick={this.makeMusic()}
-                >
-                    Submit
-                </Button>
+                { !this.state.musicReady && (
+                    <Button
+                        variant="contained"
+                        onClick={this.changeWordsToNotes}
+                    >
+                        Submit
+                    </Button>
+                )}
+                { this.state.musicReady && (
+                    <Fragment>
+                        <Button
+                            variant="contained"
+                            onClick={this.playMusic}
+                        >
+                            Play
+                        </Button>
+                        <Button
+                            variant="contained"
+                            onClick={this.stopMusic}
+                        >
+                            Stop
+                        </Button>
+                        <Button
+                            variant="contained"
+                            onClick={this.resetMusic}
+                        >
+                            Reset
+                        </Button>
+                    </Fragment>
+                )}
             </Fragment>
         );
     }
